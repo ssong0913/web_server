00590f36d17b716b0451b0e7702a8a8b3c24813e
Your Name <you@example.com>
2023-07-14 17:39:46 +0900

    0714

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    
+    print(dict(request.headers))
     return render_template('index.html')
 
 if __name__ == '__main__':
