b4cf8623938144cc9b90ec1717576e66882b1cdc
Your Name <you@example.com>
2023-07-17 17:47:33 +0900

    0717 get post

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 from flask import Flask , render_template, request
+from data import Articles
+# print(Articles)
+
 
 app = Flask(__name__)
 
 @app.route('/' , methods=['GET','POST'])
 def index():
-    if request.method == "GET":
+    """ if request.method == "GET":
         os_info = dict(request.headers)
         print(os_info)
-        return render_template('index.html', header = os_info)
+        name = request.args.get("name")
+        print(name)
+        hello = request.args.get("hello")
+        print(hello)
+        return render_template('index.html', header = f'{name}님 {hello}!!')
                 #jinja2 문법
     elif request.method == "POST":
-        return render_template('index.html', header = "안녕하세요")
+        data = request.form.get("name")
+        data_2 = request.form['hello']
+        print(f'{name}님 {hello}!!')
+        return render_template('index.html', header = "안녕하세요adsfasdf") """
+    
+    return render_template('index.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == "GET":
+        return render_template('hello.html')
+    
+    elif request.method == "POST":
+        name = request.form['name']
+        hello = request.form['hello']
+        return render_template('index.html', name = name, hello = hello)
+    
+@app.route('/list', methods=['GET', 'POST'])
+def list():
+    data = Articles()
+    return render_template('list.html', data = data )
 
 if __name__ == '__main__':
     app.run(debug=True)
