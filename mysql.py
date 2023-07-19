c1ac7d212b09d8ac9c8fbe766f4344316f25c728
Your Name <you@example.com>
2023-07-19 15:31:03 +0900

    session 처리

--- mysql.py
+++ mysql.py
@@ -49,28 +49,30 @@ class Mysql:
 
         return result
 
-    def verify_password(self, email, password):
-        db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
-        curs = db.cursor()
+    def verify_password(self, password, hashed_password):
+        # db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
+        # curs = db.cursor()
 
-        sql = f'SELECT * FROM user WHERE email = %s;'
+        # sql = f'SELECT * FROM user WHERE email = %s;'
         
-        curs.execute(sql, email) 
+        # curs.execute(sql, email) 
 
-        rows = curs.fetchall()
-        print(rows)
-        # db.commit()
-        db.close()
+        # rows = curs.fetchall()
+        # print(rows)
+        # # db.commit()
+        # db.close()
 
-        if len(rows) != 0:
-            hashed_password = rows[0][4]
-            result = check_password(password, hashed_password)
-            if result:
-                print("Welcome to My World")
-            else:
-                print("MissMatch Password")
-        else:
-            print("User is not founded")
+        # if len(rows) != 0:
+        #     hashed_password = rows[0][4]
+        #     result = check_password(password, hashed_password)
+        #     if result:
+        #         print("Welcome to My World")
+        #     else:
+        #         print("MissMatch Password")
+        # else:
+        #     print("User is not founded")
+        result = check_password(password, hashed_password)
+        return result
 
     def del_user(self, email):
         db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
